813127b682d8bd904188347b9f7f4f31c964399b
SSSAIVIDULA <155509695+SSSAIVIDULA@users.noreply.github.com>
2026-09-12 13:04:28 +0530

    Time: 292 ms (83.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,15 @@
+select sample_id,dna_sequence,species,
+case
+when dna_sequence like "ATG%" then 1 else 0 
+end as has_start,
+case
+when dna_sequence like '%TAA' OR dna_sequence like  '%TAG' OR dna_sequence like '%TGA' then 1 else 0
+end as has_stop,
+case
+when dna_sequence like '%ATAT%' then 1 else 0
+end as has_atat,
+case 
+when dna_sequence like '%ggg%' then 1 else 0
+end as has_ggg 
+from Samples
+order by sample_id asc;
